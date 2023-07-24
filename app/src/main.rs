cf7dd390c282d6aa7704f603301acee17b304222
Takaharu Nakamura <64473501+appare45@users.noreply.github.com>
2023-07-25 01:49:42 +0900

    implemented reading config file

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let c =read_config_file();
+    print!("{:?}", c);
+}
+
+fn read_config_file() -> File {
+    let f = match File::open("config.json") {
+        Ok(file) => file,
+        Err(e) => panic!("Error opening file: {:?}", e)
+    };
+    return f;
 }
