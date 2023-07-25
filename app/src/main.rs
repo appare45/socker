7cb42a83f54f0f2e55b61b272466bcb3ee884f86
Takaharu Nakamura <64473501+appare45@users.noreply.github.com>
2023-07-25 09:01:20 +0900

    added file validation

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,9 +6,22 @@ fn main() {
 }
 
 fn read_config_file() -> File {
-    let f = match File::open("config.json") {
+    let file = match File::open("config.json") {
         Ok(file) => file,
         Err(e) => panic!("Error opening file: {:?}", e)
     };
-    return f;
+
+    // validate filetype
+    let metadata = match file.metadata() {
+        Ok(m) => m,
+        Err(e) => panic!("Error reading metadata: {:?}", e)
+    };
+    if !metadata.is_file() {
+        panic!("Error: config.json is not a file");
+    }
+    if metadata.len() == 0 {
+        panic!("Error: config.json is empty");
+    }
+
+    return file;
 }
