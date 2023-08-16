c2a1f6bf361d5936b678ce698cb245e7872d52bf
Takaharu Nakamura <64473501+appare45@users.noreply.github.com>
2023-08-16 16:24:20 +0900

    separate files

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,48 +1,10 @@
-use std::fs::File;
-use std::io::BufReader;
-use serde_json;
-use serde::Deserialize;
+use crate::ps::ps;
+
+mod parse;
+mod ps;
 
 fn main() {
-    let c = read_config_file();
-    let config = parse_config_file(&c);
+    let config = parse::Config::new();
     println!("{}", config.ociVersion);
-}
-
-fn read_config_file() -> File {
-    let file = match File::open("config.json") {
-        Ok(file) => file,
-        Err(e) => panic!("Error opening file: {:?}", e),
-    };
-
-    // validate filetype
-    let metadata = match file.metadata() {
-        Ok(m) => m,
-        Err(e) => panic!("Error reading metadata: {:?}", e),
-    };
-
-    if !metadata.is_file() {
-        panic!("Error: config.json is not a file");
-    }
-
-    if metadata.len() == 0 {
-        panic!("Error: config.json is empty");
-    }
-
-    return file;
-}
-
-// https://github.com/opencontainers/runtime-spec/blob/main/config.md
-#[derive(Deserialize)]
-struct Config {
-    ociVersion: String,
-}
-
-fn parse_config_file(file: &File) -> Config {
-    let reader = BufReader::new(file);
-    let c = match serde_json::from_reader(reader) {
-        Ok(config) => config,
-        Err(e) => panic!("Error desirializeing config.json: {:?}", e)
-    };
-    return c;
+    ps();
 }
