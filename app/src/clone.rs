38aee39e372eacf4f40f493ebd73cb86ab142f02
appare45 <dev@appare45.com>
2023-12-13 15:56:59 +0900

    Check NIC

--- app/src/clone.rs
+++ app/src/clone.rs
@@ -1,17 +1,16 @@
 use nix::{
+    ifaddrs,
     libc::{self, SIGCHLD},
     sched::{clone, CloneFlags},
     sys::wait::waitpid,
-    unistd::{chroot, execvp, chdir},
+    unistd::{chdir, chroot, execvp},
 };
+
+use rtnetlink::LinkAddRequest;
 
 use crate::mount::mount::mount_bind;
 
-use std::{
-    ffi::CString,
-    path::Path,
-};
-
+use std::{ffi::CString, path::Path};
 
 pub fn new_uts() {
     let root = Path::new("./test-new");
@@ -22,6 +21,21 @@ pub fn new_uts() {
         println!("new root from: {}", root.display());
         chroot(root).unwrap();
         chdir("/").unwrap();
+        let addrs = ifaddrs::getifaddrs().unwrap();
+        for ifaddr in addrs {
+            match ifaddr.address {
+                Some(address) => {
+                    println!("interface {} address {}", ifaddr.interface_name, address);
+                }
+                None => {
+                    println!(
+                        "interface {} with unsupported address family",
+                        ifaddr.interface_name
+                    );
+                }
+            }
+        }
+
         let cmd = CString::new("bash").unwrap();
         let args = vec![CString::new("containered bash").unwrap()];
         match execvp(&cmd, &args) {
@@ -35,7 +49,10 @@ pub fn new_uts() {
 
     let cb = Box::new(closure);
     let mut stack = [0u8; 8192];
-    let flags = CloneFlags::CLONE_NEWUTS | CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_FILES;
+    let flags = CloneFlags::CLONE_NEWUTS
+        | CloneFlags::CLONE_NEWNS
+        | CloneFlags::CLONE_FILES
+        | CloneFlags::CLONE_NEWNET;
     let signal = SIGCHLD as libc::c_int;
 
     let pid = clone(cb, &mut stack, flags, Some(signal));
@@ -44,5 +61,4 @@ pub fn new_uts() {
     while let Ok(status) = waitpid(None, None) {
         println!("Exit Status: {:?}", status)
     }
-
 }
