bb655863a9543dea9ea614c94cb62a16aff269ad
appare45 <kaibatsu35.7m45@gmail.com>
2025-11-28 11:58:30 +0900

    set user ns
    appare45@lima-devenv:~/ghq/github.com/appare45/socker$ cargo run
       Compiling socker v0.1.0
    (/home/appare45.linux/ghq/github.com/appare45/socker)
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.55s
         Running `target/debug/socker`
    Hello, world!
    Spawned child process with PID: 51395
    In child process with PID: 1
    Child before mapping UID: 65534, GID: 65534
    Parent UID: 501, GID: 1000
    Writing to pipe...
    Read: Hello from parent process!
    
    Child UID: 0, GID: 0
    Child process 51395 exited with status: Exited(Pid(51395), 0)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::{
+    fs,
     io::{self, PipeReader, Read, Write},
     os::fd::{AsRawFd, FromRawFd},
 };
 
-use nix::{libc::SIGCHLD, unistd::close};
+use nix::{
+    libc::SIGCHLD,
+    unistd::{Gid, Uid, close, setgid, setuid},
+};
 use socker::namespace::flags::NamespaceFlags;
 
 fn child_fn(readfd: i32, writefd: i32) {
@@ -11,15 +15,23 @@ fn child_fn(readfd: i32, writefd: i32) {
     println!("In child process with PID: {}", pid);
     let mut reader = unsafe { PipeReader::from_raw_fd(readfd) };
     close(writefd).expect("Failed to close write end of pipe in child");
+    let uid = nix::unistd::getuid();
+    let gid = nix::unistd::getgid();
+    println!("Child before mapping UID: {}, GID: {}", uid, gid);
     let mut read = [0_u8; 128];
     reader.read(&mut read).unwrap();
     println!("Read: {}", String::from_utf8_lossy(&read));
+    setuid(Uid::from_raw(0)).expect("Failed to set UID to 0");
+    setgid(Gid::from_raw(0)).expect("Failed to set GID to 0");
+    let uid = nix::unistd::getuid();
+    let gid = nix::unistd::getgid();
+    println!("Child UID: {}, GID: {}", uid, gid);
 }
 const CHILD_STACK_SIZE: usize = 1024 * 1024;
 
 fn main() {
     println!("Hello, world!");
-    let flags = socker::namespace::Namespace::new(NamespaceFlags::empty());
+    let flags = socker::namespace::Namespace::new(NamespaceFlags::USER | NamespaceFlags::PID);
     let (reader, mut writer) = io::pipe().expect("Failed to create pipe");
     let stack: &mut [u8; CHILD_STACK_SIZE] = &mut [0; CHILD_STACK_SIZE];
     let pid = match unsafe {
@@ -42,6 +54,24 @@ fn main() {
             return;
         }
     };
+    let uid = nix::unistd::getuid();
+    let gid = nix::unistd::getgid();
+    println!("Parent UID: {}, GID: {}", uid, gid);
+    fs::write(format!("/proc/{}/setgroups", pid), "deny\n").expect("Failed to write setgroups");
+    // GID map
+    fs::write(
+        format!("/proc/{}/gid_map", pid),
+        format!("0 {} 1\n", gid.as_raw()),
+    )
+    .expect("Failed to write gid_map");
+    // UID map
+    fs::write(
+        format!("/proc/{}/uid_map", pid),
+        format!("0 {} 1\n", uid.as_raw()),
+    )
+    .expect("Failed to write uid_map");
+
+    println!("Writing to pipe...");
     writer
         .write_all(b"Hello from parent process!\n")
         .expect("Failed to write to pipe");
