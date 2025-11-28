255405cd2e6f6d98cdeeba0bb885dd250e130bc4
appare45 <kaibatsu35.7m45@gmail.com>
2025-11-28 11:58:30 +0900

    namespace separation from scratch

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod namespace;
+pub mod process;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use socker::{namespace::flags::NamespaceFlags, process::clone};
+
+fn child_fn() {
+    let pid = nix::unistd::getpid();
+    println!("In child process with PID: {}", pid);
+}
+
 fn main() {
     println!("Hello, world!");
+    let flags = socker::namespace::Namespace::new(NamespaceFlags::PID);
+    match clone(child_fn, flags.proc_clone_flags()) {
+        Ok(child_pid) => {
+            println!("Spawned child process with PID: {}", child_pid);
+        }
+        Err(e) => {
+            eprintln!("Failed to clone process: {}", e);
+        }
+    }
 }
